4fb459a9c000669666377970b98456353210be52
Artem Frolov <artfrol.af@gmail.com>
2025-05-05 21:42:42 +0300

    host on gh pages and redirect to datalyzed.ru

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const baseName =
+  import.meta.env.MODE === 'development' || window.location.hostname === 'datalyzed.ru'
+    ? '/'
+    : '/datalyzed-website';
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename={baseName}>
+    <App />
+  </BrowserRouter>
+);
